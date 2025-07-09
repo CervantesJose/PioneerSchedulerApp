69e39c7e4740197a47cb35e03f39c0c7006a146a
CervantesJose <cervantesj1077@gmail.com>
2025-07-09 15:53:33 -0600

    Adding DateRangeSelectorView, basic view as of now

---
+++ PioneerScheduler/View/HelperViews/DateRangeSelectorView.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarRangeView.swift
+//  PioneerScheduler
+//
+//  Created by Jose Cervantes on 7/9/25.
+//
+
+import Combine
+import SwiftUI
+
+struct DateRangeSelectorView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DateRangeSelectorView()
+}
