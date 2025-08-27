1a2a7073199e8cb675f17b045dd3bed815acca1a
CervantesJose <cervantesj1077@gmail.com>
2025-08-27 16:48:04 -0600

    Successfully implemented PDF exporting and sharing

--- PioneerScheduler/View/Timesheet/TimesheetDetailView.swift
+++ PioneerScheduler/View/Timesheet/TimesheetDetailView.swift
@@ -11,8 +11,9 @@ struct TimesheetDetailView: View {
     @Environment(\.dismiss) var dismiss
     @StateObject private var viewModel: DetailViewModel
     let onSave: (TimesheetWithWorkdays) -> Void
-
     let timesheet: TimesheetWithWorkdays
+
+    @State private var exportURL: URL?
 
     init(timesheet: TimesheetWithWorkdays, onSave: @escaping (TimesheetWithWorkdays) -> Void) {
         self.timesheet = timesheet
@@ -42,8 +43,18 @@ struct TimesheetDetailView: View {
                 }
             }
             
-            Section(header: Text("Total hours")) {
-                Text(viewModel.totalDuration.asHourMinuteString)
+            Section(header: Text("Total time: \(viewModel.totalDuration.asHourMinuteString)")) {
+                VStack {
+                    if let url = exportURL {
+                        ShareLink("Export PDF", item: url)
+                    } else {
+                        Button {
+                            exportURL = renderPDF(timesheet: timesheet, workdays: viewModel.workdays)
+                        } label: {
+                            Label("Export PDF", systemImage: "square.and.arrow.up")
+                        }
+                    }
+                }
             }
         }
         .navigationTitle(title)
@@ -63,6 +74,75 @@ struct TimesheetDetailView: View {
                 }
             }
         }
+    }
+
+    @MainActor
+    func renderPDF(timesheet: TimesheetWithWorkdays, workdays: [Workday]) -> URL? {
+        // 1: Render view
+        let view = TimesheetPDFView(timesheet: timesheet, workdays: workdays)
+        let renderer = ImageRenderer(content: view)
+
+        // 2: Save it to our documents directory
+        let url = URL.documentsDirectory.appending(path: "timesheet.pdf")
+
+        // 3: Start the rendering process
+        renderer.render { size, context in
+            // 4: Tell SwiftUI our PDF should be the same size as the views we're rendering
+            var box = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+
+            // 5: Create the CGContext for our PDF pages
+            guard let pdf = CGContext(url as CFURL, mediaBox: &box, nil) else { return }
+
+            // 6: Start a new PDF page
+            pdf.beginPDFPage(nil)
+
+            // 7: Render the SwiftUI view data onto the page
+            context(pdf)
+
+            // 8: End the page and close the file
+            pdf.endPDFPage()
+            pdf.closePDF()
+        }
+
+        return url
+    }
+}
+
+struct TimesheetPDFView: View {
+    let timesheet: TimesheetWithWorkdays
+    let workdays: [Workday]
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("\(timesheet.startDate.formatted(date: .numeric, time: .omitted)) - \(timesheet.startDate.formatted(date: .numeric, time: .omitted))")
+                .font(.title)
+                .bold()
+
+            Divider()
+
+            ForEach(workdays) { workday in
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(workday.date.formatted(date: .abbreviated, time: .omitted))
+                        .font(.headline)
+
+                    if workday.tasks.isEmpty == false {
+                        Text(workday.tasks.joined(separator: ", "))
+                            .font(.subheadline)
+                    }
+
+                    Text("Time worked: \(workday.duration.asHourMinuteString)")
+                        .font(.subheadline)
+                        .italic()
+                }
+                .padding(.vertical, 4)
+            }
+
+            Divider()
+
+            Text("Total hours: \(workdays.reduce(0) { $0 + $1.duration }.asHourMinuteString)")
+                .font(.headline)
+        }
+        .padding()
     }
 }
 
